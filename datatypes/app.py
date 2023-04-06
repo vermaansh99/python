7c234187c0e1c1c793f9310153eb0b7a89652c4f
vermaansh99 <vermaansh99@gmail.com>
2023-04-07 02:39:41 +0530

    datatypes added

---
+++ datatypes/app.py
@@ -0,0 +1,24 @@
+age = 25  # integers
+
+name = "Sumit"  # strings
+
+isRainy = True
+
+number = 3+5j
+
+cgpa = 6.2
+
+box = 12
+
+print(name)
+
+name = "Sameer"
+
+print(name)
+
+name = "sumit"
+age = 25
+age = str(25)
+print("Sumit is "+age+"Years old")
+
+salary = 35000
